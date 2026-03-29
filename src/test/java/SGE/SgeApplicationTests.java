ff014bde83360d6ce690adf9e905a612e44159c2
Helena <estrelahelena7@gmail.com>
2026-03-29 16:52:54 -0300

    Estrutura Básica

---
+++ src/test/java/SGE/SgeApplicationTests.java
@@ -0,0 +1,13 @@
+package SGE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
